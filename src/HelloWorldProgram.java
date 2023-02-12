041e3910c62ee66ea8078681c8424dbd6c26a676
DineshSD432 <dinesh.s9150@gmail.com>
2023-02-12 18:53:38 +0530

    Simple Changes for learning purpose

--- src/HelloWorldProgram.java
+++ src/HelloWorldProgram.java
@@ -3,6 +3,7 @@
 class HelloWorldProgram{
     public static void main(String[] args) {
         System.out.println("Hello World From Java");
-        System.out.println("The First");
+        System.out.println("The First Change");
+        System.out.println("The Second Change  ");
     }
 }
